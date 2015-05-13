7d2667866d005da12be58164f98766ec4ac1311f
Yehor Nazarkin <y_nazarkin@wargaming.net>
2015-05-14 02:28:39 +0300

    Added tree traversal method

--- tree.erl
+++ tree.erl
@@ -22,6 +22,24 @@ lookup(Key, {node, {NodeKey, _, Smaller, _}}) when Key < NodeKey ->
 lookup(Key, {node, {_, _, _, Larger}}) ->
     lookup(Key, Larger).
 
+
+has_value(Val, Tree) ->
+    try has_value1(Val, Tree) of
+        false -> false
+    catch
+        true -> true
+    end
+.
+
+has_value1(_, {node, 'nil'}) ->
+    false;
+has_value1(Val, {node, {_, Val, _, _}}) ->
+    throw(true);
+has_value1(Val, {node, {_, _, Left, Right}}) ->
+    has_value1(Val, Left),
+    has_value1(Val, Right)
+.
+
 % {node,
 %     {Key, Value,
 %     {node,
